935b8d3e058929d5558320f959f68914f3e3c43a
Raven McKnight <raven.mcknight@metrotransit.org>
2019-10-04 10:46:03 -0500

    update & improve census pulls

--- get-census.R
+++ get-census.R
@@ -2,7 +2,7 @@
 
 ## packages -----------------------------------------------
 
-packages <- c('data.table', 'tigris', 'tidycensus', 'purrr')
+packages <- c('data.table', 'tigris', 'tidycensus', 'purrr', 'tidyr', 'ggplot2')
 
 miss_pkgs <- packages[!packages %in% installed.packages()[,1]]
 
@@ -20,7 +20,7 @@ options(tigris_class = 'sf')
 ## acs variables ------------------------------------------
 
 # look at acs 5yr estimates for 2015
-v15 <- load_variables(2016, "acs5", cache = TRUE)
+v15 <- load_variables(2015, "acs5", cache = TRUE)
 setDT(v15)
 View(v15)
 
@@ -36,10 +36,8 @@ basics <- map_dfr(
   ~ get_acs(
     geography = "block group",
     variables = c(tot_pop = "B01003_001",
-                  tot_no_veh = "B08201_002",
                   median_age = "B01002_001",
                   median_hh_income = "B19013_001",
-                  speak_only_english = "B16001_002",
                   white_alone = "B02001_002"),
     state = "MN",
     county = counties,
@@ -49,7 +47,121 @@ basics <- map_dfr(
   .id = "year"
 )
 
+basics <- basics %>% pivot_wider(names_from = 'variable', values_from = c('estimate', 'moe'))
+setDT(basics)
+basics[, perc_only_white := estimate_white_alone/estimate_tot_pop]
+head(basics)
+
 saveRDS(basics, 'data/covariates/basic_acs.RDS')
+
+## language spoken at home ----------------------
+
+language <- map_dfr(
+  years,
+  ~ get_acs(
+    geography = "block group",
+    variables = c(total_517 = "B16004_002",
+                  speak_only_english_517 = "B16004_003",
+                  total_1864 = "B16004_024",
+                  speak_only_english_1864 = "B16004_025",
+                  tot_65 = "B16004_046", 
+                  speak_only_english_65 = "B16004_047"),
+    state = "MN",
+    county = counties,
+    year = .x,
+    survey = "acs5"
+  ),
+  .id = "year"
+)
+
+language <- language %>% pivot_wider(names_from = variable, values_from = c('estimate', 'moe'))
+setDT(language)
+language[, est_tot_over5 := estimate_total_517 + estimate_total_1864 + estimate_tot_65]
+language[, est_tot_eng_only := estimate_speak_only_english_517 + estimate_speak_only_english_1864 + estimate_speak_only_english_65]
+language[, perc_english_only := est_tot_eng_only/est_tot_over5]
+
+# mostly want the last column but should save everything because of MOEs
+saveRDS(language, 'data/covariates/language.RDS')
+
+# these are pretty big moes
+ggplot(language, aes(x=moe_speak_only_english_1864/estimate_speak_only_english_1864)) + geom_histogram()
+
+## educational attainment -----------------------
+
+education <- map_dfr(
+  years,
+  ~ get_acs(
+    geography = "block group",
+    variables = c(total_pop_25 = "B15003_001", 
+                  highschool = "B15003_017", 
+                  highschool_equiv = "B15003_018",
+                  bachelors = "B15003_022"),
+    state = "MN",
+    county = counties,
+    year = .x,
+    survey = "acs5"
+  ),
+  .id = "year"
+)
+
+education <- education %>% pivot_wider(names_from = variable, values_from = c('estimate', 'moe'))
+setDT(education)
+education[, hs_or_equiv := estimate_highschool + estimate_highschool_equiv]
+education[, perc_hs := hs_or_equiv/estimate_total_pop_25]
+education[, perc_bach := estimate_bachelors/estimate_total_pop_25]
+saveRDS(education, 'data/covariates/education.RDS')
+
+
+# native/foreign born at CENSUS TRACT
+nativity <- map_dfr(
+  years,
+  ~ get_acs(
+    geography = "tract",
+    variables = c(total_pop = "B05012_001",
+                  tot_native = "B05012_002",
+                  tot_foreign = "B05012_003"),
+    state = "MN",
+    county = counties,
+    year = .x,
+    survey = "acs5"
+  ),
+  .id = "year"
+)
+
+nativity <- nativity %>% pivot_wider(names_from = variable, values_from = c('estimate', 'moe'))
+setDT(nativity)
+nativity[, perc_native := estimate_tot_native/estimate_total_pop]
+nativity[, perc_foreign := estimate_tot_foreign/estimate_total_pop]
+
+saveRDS(nativity, 'data/covariates/nativity.RDS')
+
+# housing tenure & vehicle availability ---------
+
+ten_veh <- map_dfr(
+  years,
+  ~ get_acs(
+    geography = "block group",
+    variables = c(total_pop = "B25044_001",
+                  total_own = "B25044_002",
+                  own_no_veh = "B25044_003",
+                  total_rent = "B25044_009",
+                  rent_no_veh = "B25044_010"),
+    state = "MN",
+    county = counties,
+    year = .x,
+    survey = "acs5"
+  ),
+  .id = "year"
+)
+
+ten_veh <- ten_veh %>% pivot_wider(names_from = variable, values_from = c('estimate', 'moe'))
+setDT(ten_veh)
+ten_veh[, perc_owner_occ := estimate_total_own/estimate_total_pop]
+ten_veh[, perc_rent := estimate_total_rent/estimate_total_pop]
+ten_veh[, tot_no_veh := estimate_own_no_veh + estimate_rent_no_veh]
+ten_veh[, perc_no_veh := tot_no_veh/estimate_total_pop]
+
+saveRDS(ten_veh, 'data/covariates/housing-and-vehicles.RDS')
 
 ## employment data ----------------------------------------
 
@@ -66,3 +178,66 @@ tot_jobs <- readRDS('/Users/raven/Documents/projects/LODES-analysis/data/tot_job
 setDT(tot_jobs)
 tot_jobs <- tot_jobs[year %in% years]
 saveRDS(tot_jobs, 'data/covariates/tot_jobs.RDS')
+
+# maybe better is rac data
+
+## get RAC data -------------------------------------------
+
+# residence area characteristics
+# type = JT00 is all jobs
+# year = most recent year
+
+rac_urls <- c('https://lehd.ces.census.gov/data/lodes/LODES7/mn/rac/mn_rac_S000_JT00_2017.csv.gz',
+              'https://lehd.ces.census.gov/data/lodes/LODES7/mn/rac/mn_rac_S000_JT00_2016.csv.gz',
+              'https://lehd.ces.census.gov/data/lodes/LODES7/mn/rac/mn_rac_S000_JT00_2015.csv.gz')
+
+for(i in 1:length(years)){
+  download.file(rac_urls[i], paste0('data/covariates/rac/mn_rac_', years[i], '.csv.gz'))
+}
+
+rac_files <- list.files(path = 'data/covariates/rac/')
+l <- lapply(paste0('data/covariates/rac/', rac_files), fread)
+
+l[[1]]$year <- 2017
+l[[2]]$year <- 2016
+l[[3]]$year <- 2015
+
+for(i in 1:3){
+  colnames(l[[i]]) <- c('h_geocode', 'tot_jobs', 'age_29', 'age_30_54', 'age_55',
+                      'wage_1250', 'wage_1250_3333', 'wage_3333', 'ag', 'mining',
+                      'utils', 'construction', 'manuf', 'whole_trade', 'ret_trade',
+                      'transp', 'info', 'finance', 'real_estate', 'prof_serv', 
+                      'manage', 'admin', 'educ', 'health', 'arts', 'ac_food', 
+                      'other', 'pub_admin', 'white_alone', 'black', 'amind', 
+                      'asian', 'native', 'two_or_more', 'not_hisp', 'hisp', 
+                      'less_than_hs', 'hs', 'some_college', 'degree', 'male', 'female', 
+                      'createdate', 'year')
+}
+
+# i think i'll mostly be interested in the service industry
+
+# aggregate blocks to bgs
+
+for(i in 1:3){
+  l[[i]][, GEOID := substr(h_geocode, 1, 12)]
+}
+
+for(i in 1:3){
+  l[[i]]$createdate <- NULL
+  l[[i]] <- l[[i]][, lapply(.SD, sum, na.rm=TRUE), by= c('GEOID', 'year')]
+}
+
+# restrict to metro area
+bgs <- block_groups('MN', counties, year = 2016)
+
+for(i in 1:3){
+  l[[i]] <- l[[i]][GEOID %in% bgs$GEOID]
+}
+
+saveRDS(l[[1]], 'data/covariates/rac/rac-2017.RDS')
+saveRDS(l[[2]], 'data/covariates/rac/rac-2016.RDS')
+saveRDS(l[[3]], 'data/covariates/rac/rac-2015.RDS')
+
+# oh duh should just save the whole thing
+rac <- rbindlist(l)
+saveRDS(rac, 'data/covariates/rac/all-rac.RDS')
