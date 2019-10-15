5d13d7843773db455c9cef7043bab2b92ab86346
Raven <raven.i.mcknight@gmail.com>
2019-10-15 16:38:31 -0500

    add more reproducible script to get data for modeling

---
+++ get-modeling-data.R
@@ -0,0 +1,57 @@
+## Goal: fix my terrible practices which have lead to totally irreproducible data! woo!
+
+
+packages <- c('data.table', 'tigris', 'ggplot2')
+
+miss_pkgs <- packages[!packages %in% installed.packages()[,1]]
+
+if(length(miss_pkgs) > 0){
+  install.packages(miss_pkgs)
+}
+
+invisible(lapply(packages, library, character.only = TRUE))
+
+rm(miss_pkgs, packages)
+
+options(tigris_class = 'sf')
+
+
+## Get 2017 average daily activity --------------
+
+apc <- readRDS('/Users/raven/Documents/honors/honors-work/data/mt-data/apc-bg-ag-sum.RDS')
+setDT(apc)
+
+# restrict to 2017
+apc <- apc[date_key %like% 2017]
+apc <- apc[, .(avg_activity = mean(ag_board + ag_alight)), by = GEOID]
+
+# add basic acs
+acs <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/basic_acs.RDS')
+setDT(acs)
+acs <- acs[year == 3]
+apc <- apc[acs, on = 'GEOID']
+
+apc[, avg_act_per_capita := mean(avg_activity/estimate_tot_pop), by = 'GEOID']
+
+educ <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/education.RDS')
+house_veh <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/housing-and-vehicles.RDS')
+language <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/language.RDS')
+nativity <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/nativity.RDS')
+employment <- readRDS('/Users/raven/Documents/honors/honors-work/data/covariates/wac/wac-2017.RDS')
+setDT(educ)
+setDT(house_veh)
+setDT(language)
+setDT(nativity)
+setDT(employment)
+
+mod_dat <- apc[educ[year == 3, c('perc_hs', 'perc_bach', 'GEOID')], on = 'GEOID']
+mod_dat <- mod_dat[house_veh[year == 3, c('GEOID', 'perc_rent', 'perc_owner_occ', 'perc_no_veh')], on = 'GEOID']
+mod_dat <- mod_dat[language[year == 3, c('GEOID', 'perc_english_only')], on = 'GEOID']
+
+setnames(nativity, 'GEOID', 'tract_GEOID')
+mod_dat[, tract_GEOID := substr(GEOID, 1, 11)]
+
+mod_dat <- mod_dat[nativity[year == 3, c('tract_GEOID', 'perc_native', 'perc_foreign')], on = 'tract_GEOID']
+mod_dat <- mod_dat[employment[, c('GEOID', 'total_jobs_here')], on = 'GEOID']
+
+saveRDS(mod_dat, 'data/ag-2017-mod-dat.RDS')
