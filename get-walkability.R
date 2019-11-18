ab68932982f4c70ab80f39c18f8d7ca3dd7e8267
Raven McKnight <raven.mcknight@metrotransit.org>
2019-11-18 09:45:40 -0600

    update walkability pull

--- get-walkability.R
+++ get-walkability.R
@@ -3,10 +3,14 @@ library(sf)
 library(tigris)
 library(data.table)
 library(otptools)
+library(dplyr)
+library(ggplot2)
+
+options(tigris_class = "sf")
 
 # first, download population-weighted centroids
 url <- "https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG27.txt"
-loc <- file.path("/Users/raven/Documents/honors/honors-work/data/isochrones", 'centrs.csv')
+loc <- file.path("/Users/mcknigri/Documents/honors/honors-work/data/isochrones", 'centrs.csv')
 download.file(url, loc)
 
 centrs <- read_csv('data/isochrones/centrs.csv')
@@ -26,5 +30,23 @@ isos <- queryIsochrone(location = centrs_loc,
                        otp_params = otp_params(cutoffSec = c(600), mode = "WALK"),
                        host = "localhost", port = 8080)
 
-saveRDS(isos, "data/isochrones/centroid_isos.RDS")
-                     
+isos <- isos %>%
+  mutate(area = as.numeric(st_area(geometry)), 
+         GEOID = id)
+
+# messy but works 
+isos_dt <- as.data.frame(isos)
+isos_bg <- left_join(bgs, isos_dt[, c('area', 'GEOID')], by = 'GEOID')
+setDT(isos_bg)
+isos_bg[is.na(isos_bg$area), area := 0] 
+isos_bg <- st_as_sf(isos_bg)
+
+ggplot(isos_bg) +
+  geom_sf(aes(fill = area), lwd = 0) +
+  scale_fill_viridis_c()
+
+ggplot(isos_bg) +
+  geom_sf(aes(fill = COUNTYFP), lwd = 0) +
+  scale_fill_viridis_d()
+
+saveRDS(isos_bg, "data/isochrones/centroid_isos.RDS")
